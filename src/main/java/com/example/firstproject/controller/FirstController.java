c2e7a3435691ed7e77f809a8dac8681db77439c8
kyscodeman <metalgos2@gmail.com>
2023-09-08 05:04:11 +0900

    기본 컨트롤러 생성 및 머스태치 문법 시험

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,19 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+
+        model.addAttribute("username","홍팍");
+
+        return "greetings";
+
+    }
+}
