7d5dbc21d65eeccb915a727987e8ba878c7ed064
kyscodeman <metalgos2@gmail.com>
2023-09-09 21:53:21 +0900

    롬복과 로깅 기능을 이용한 어노테이션 활용

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j//로깅을 위한 어노테이션
 @Controller
 public class ArticleController {
 
@@ -19,18 +21,21 @@ public class ArticleController {
         return "articles/new";
     }
 
+
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
 
-        System.out.println(form.toString());
+        //System.out.println(form.toString()); 로깅으로 대채
+
+        log.info(form.toString());
         //1.dto 를 엔티티로변환!!
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
 
         //2.repository에게 엔티티들 디비에 저장
 
        Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
     }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,10 +1,14 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+@AllArgsConstructor
+@ToString
 @Entity // 엔티티 어노테이션이 있어야 db가 인식함
 public class Article {
     @Id // db의 대표값 지정
@@ -16,18 +20,5 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
