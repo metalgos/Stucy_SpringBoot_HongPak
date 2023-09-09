a32fcdf4583b03a41cbb567f41b27bb57b24ed97
kyscodeman <metalgos2@gmail.com>
2023-09-09 13:59:15 +0900

    jpa이용하여 인터페이스 작성 db세이브  및 mysql 연동(유투브 없이따로)

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,9 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ArticleController {
 
+
+    @Autowired // 객체 주입
+    private ArticleRepository articleRepository;
     @GetMapping("/articles/new")
     public String newArticleForm() {
-
         return "articles/new";
     }
 
@@ -18,6 +23,15 @@ public class ArticleController {
     public String createArticle(ArticleForm form) {
 
         System.out.println(form.toString());
+        //1.dto 를 엔티티로변환!!
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        //2.repository에게 엔티티들 디비에 저장
+
+       Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+
         return "";
     }
 
